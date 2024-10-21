13f972fe102b2a08d975348ffd182f1127d7032b
gkirber <george.kirber@gmail.com>
2024-10-21 17:38:43 +0200

    Skills style

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,5 +1,25 @@
 import styled from "styled-components";
 
 export const SectionTitle = styled.h2`
-    color: red;
+    text-align: center;
+    font-family: 'Popins', sans-serif;
+    font-weight: 600;
+    font-size: 36px;
+    letter-spacing: 0.5px;
+    margin-bottom: 90px;
+    
+    position: relative;
+    
+    &::before {
+        content: "";
+        display: inline-block;
+        width: 55px;
+        height: 1px;
+        background-color: cornflowerblue;
+        
+        position: absolute;
+        left: 50%;
+        bottom: -30px;
+        transform: translateX(-50%);
+    }
 `
--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,24 +3,26 @@ import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
+import {Container} from "../../../components/Container";
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>Skills</SectionTitle>
-            <FlexWrapper>
-                <Skill iconId={"code"} title={"html"} description={"Intermediate"}/>
-                <Skill iconId={"css"} title={"css"} description={"Intermediate"}/>
-                <Skill iconId={"react"} title={"JavaScript"} description={"Intermediate"}/>
-                <Skill iconId={"react"} title={"TypeScript"} description={"Intermediate"}/>
-                <Skill iconId={"react"} title={"React"} description={"Intermediate"}/>
-                <Skill iconId={"react"} title={"Git"} description={"Intermediate"}/>
-                <Skill iconId={"react"} title={"Styled Components"} description={"Intermediate"}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>Skills</SectionTitle>
+                <FlexWrapper>
+                    <Skill iconId={"code"} title={"html"} description={"Intermediate"}/>
+                    <Skill iconId={"css"} title={"css"} description={"Intermediate"}/>
+                    <Skill iconId={"react"} title={"JavaScript"} description={"Intermediate"}/>
+                    <Skill iconId={"react"} title={"TypeScript"} description={"Intermediate"}/>
+                    <Skill iconId={"react"} title={"React"} description={"Intermediate"}/>
+                    <Skill iconId={"react"} title={"Git"} description={"Intermediate"}/>
+                    <Skill iconId={"react"} title={"Styled Components"} description={"Intermediate"}/>
+                </FlexWrapper>
+            </Container>
+
         </StyledSkills>
     );
 };
 
-const StyledSkills = styled.section`
-    background-color: #ffe5e4;
-`
+const StyledSkills = styled.section``
